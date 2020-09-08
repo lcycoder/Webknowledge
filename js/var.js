70c0b2587821af09afa76000cde8370f41191378
lcycoder <874174364@qq.com>
2020-09-08 15:21:28 +0800

    .
    .

--- js/var.js
+++ js/var.js
@@ -36,6 +36,33 @@
 // }
 // let arr = Array.from(arrayLike);
 // console.log(arr);
-for(var i=0;i<10;i++){
-    setTimeout(console.log(i),0);//0、1、2、3、4、5、6、7、8、9
-}
+// for(var i=0;i<10;i++){
+//     setTimeout(console.log(i),0);//0、1、2、3、4、5、6、7、8、9
+// }
+// Person.prototype.name = 'li';
+// function Person(){
+
+// }
+//     var person = new Person();
+//     console.log(person.name);//li
+//     Person.prototype = { 
+//         name:"wang"
+//     } 
+    //这里Person.prototype 指向了另一个对象,而现在person的prototype指向还是之前的那个Person.prototype,相当于
+    //var a = {name:1};   
+    //var b=a;  //a和b指向了同一个地址.
+    //a={name:2};这里a改变了指向的地址,但是b没有改变,所以b还是指向原来的那个地址.
+    //console.log(b.name);//b.name=1;因为    
+    // console.log(person.name);//li
+    // var person2 = new Person();
+    // console.log(person2.name)//'wang';
+
+    var div = document.getElementById("divButtons");
+    for( var i=1;i<=10;i++){
+        var button = document.createElement("button");
+        button.innerHTML = "按钮"+i;
+        button.onclick = function(){
+            console.log(i);
+        }
+        div.appendChild(button);
+    }
