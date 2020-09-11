5a3e9acc6e6ad0fa2f2b4740f3322955ce667617
lcycoder <874174364@qq.com>
2020-09-11 14:39:57 +0800

    .

---
+++ ES6/函数/index.js
@@ -0,0 +1,11 @@
+// class animal{
+//     constructor (name,age){
+//         this.name = name;
+//         this.age = age;
+//     }
+//     print(){
+//         console.log(`${this.name}`);
+//     }
+// }
+// const a = new animal("a",18);
+// a.print();
