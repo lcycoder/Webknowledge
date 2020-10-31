dab82b658855066bc8490670ce5df8272997e4de
lcycoder <56672761+lcycoder@users.noreply.github.com>
2020-10-31 18:23:37 +0800

    补充

--- js/String对象方法.md
+++ js/String对象方法.md
@@ -12,7 +12,7 @@
 * slice()  提取指定位置的字符串
 * split () 将字符串分割成数组
 * starWith()是否以指定的字符开头
-* substr()  从索引出开始,提取指定数量的字符
+* substr()  从索引出开始,提取指定数量的字符 ([start],length)
 * substring() 提取两个索引之间的字符[start,end)
 * ToLowerCase() 
 * ToUpperCase()
--- js/var.js
+++ js/var.js
@@ -1,93 +1,34 @@
-// var str = 'abc123de45fgh6789qqq111';
-// function findNum(str){
-//     var tmp = '',
-//     arr = [];
-//     for(var i = 0;i<str.length;i++){
-//         var cur = str[i];
-//         if(!isNaN(cur)){
-//             tpm += cur
-//         }else{
-//             if(tmp){
-//                 arr.push(tmp);
-//                 tmp = '';
-//             }
-//         }
-//     }
-//     if(tmp){
-//         arr.push(tmp);
-//     }
-//     return arr;
-// }
-// console.log(findNum(str))
+var reg = /^[0-9]{1,20}$/
+var reg = /^\w|.{5,20}/ /^[a-zA-Z]{1}([a-zA-Z0-9]|)
+var reg = /^[a-zA-Z]{1,30}$/g;
+var reg = /^(\w){6,20}$/g
+var reg = /^1[3456789]d{9}&/
 
-// var str = '1223334444';
-// var reg = /\d?/g;
-// var res = str.match(reg);
-// console.log(res);
-// var str = '  我是空格君   ';
-// var reg = /^\s+|\s+$/g;
-
-// console.log(typeof NaN);
-// let arrayLike  = {
-//     0: 'tom',
-//     1: '65',
-//     2: '男',
-//     'length': 3
-// }
-// let arr = Array.from(arrayLike);
-// console.log(arr);
-// for(var i=0;i<10;i++){
-//     setTimeout(console.log(i),0);//0、1、2、3、4、5、6、7、8、9
-// }
-// Person.prototype.name = 'li';
-// function Person(){
-
-// }
-//     var person = new Person();
-//     console.log(person.name);//li
-//     Person.prototype = { 
-//         name:"wang"
-//     } 
-    //这里Person.prototype 指向了另一个对象,而现在person的prototype指向还是之前的那个Person.prototype,相当于
-    //var a = {name:1};   
-    //var b=a;  //a和b指向了同一个地址.
-    //a={name:2};这里a改变了指向的地址,但是b没有改变,所以b还是指向原来的那个地址.
-    //console.log(b.name);//b.name=1;因为    
-    // console.log(person.name);//li
-    // var person2 = new Person();
-    // console.log(person2.name)//'wang';
-
-    // var div = document.getElementById("divButtons");
-    // for( var i=1;i<=10;i++){
-    //     var button = document.createElement("button");
-    //     button.innerHTML = "按钮"+i;
-    //     button.onclick = function(){
-    //         console.log(i);
-    //     }
-    //     div.appendChild(button);
-    // }
-    // var i = ':';
-    //     var str = `name${i}  
-    //     console.log(str);
-
-    function Animal (name) {
-        this.name= name|| 'Animal';
-        this.sleep = function(){
-            console.log(this.name + '正在睡觉!');
-        };
-    }
-    //原型上面的方法:
-    Animal.prototype.eat = function(food){
-        console.log(this.name + '正在吃' + food);
-    }
-
-    原型链继承
-
-    function Dog(){
-
-    }
-    Dog.prototype = new Animal();
-    Dog.prototype.name = 'dog';
-    var dog = new Dog();
-    console.log(dog.name);
+  
+var clone = function(obj ,deep){
     
+    if(Array.isArray(obj)){
+        var newobj={};
+        if(deep)
+        for(var i=0;i<obj.length;i++){
+            newobj.push(this.clone(obj[i],deep));
+        }
+        return newoobj;
+    }
+    else if(typeof obj === "object")
+    {
+    return obj.slice();
+    else if(typeof obj === 'object'){
+        var newobj = {};
+        for(var prop in obj){
+            if(deep){
+                newobj[prop] = this.clone(obj[prop],deep);
+            }
+            else
+            newobj[prop]=obj[prop];
+        }
+        return newobj;
+    }
+    else 
+    return obj;
+}
