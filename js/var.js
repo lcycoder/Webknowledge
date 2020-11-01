5c7181c0141661ba568b66b9415f52eb46acb35e
lcycoder <56672761+lcycoder@users.noreply.github.com>
2020-11-01 20:04:32 +0800

    补充

--- js/var.js
+++ js/var.js
@@ -1,34 +1,16 @@
-var reg = /^[0-9]{1,20}$/
-var reg = /^\w|.{5,20}/ /^[a-zA-Z]{1}([a-zA-Z0-9]|)
-var reg = /^[a-zA-Z]{1,30}$/g;
-var reg = /^(\w){6,20}$/g
-var reg = /^1[3456789]d{9}&/
 
-  
-var clone = function(obj ,deep){
-    
-    if(Array.isArray(obj)){
-        var newobj={};
-        if(deep)
-        for(var i=0;i<obj.length;i++){
-            newobj.push(this.clone(obj[i],deep));
-        }
-        return newoobj;
-    }
-    else if(typeof obj === "object")
+function add(arr){
+    var sum=0;
+    for(var i =0;i<arr.length;i++)
     {
-    return obj.slice();
-    else if(typeof obj === 'object'){
-        var newobj = {};
-        for(var prop in obj){
-            if(deep){
-                newobj[prop] = this.clone(obj[prop],deep);
-            }
-            else
-            newobj[prop]=obj[prop];
-        }
-        return newobj;
+        if(Array.isArray(arr[i]))
+            sum+=add(arr[i]);
+        else
+            sum+=arr[i];
     }
-    else 
-    return obj;
+    return sum;
 }
+
+    line=read_line();
+    var num = add(line);
+	console.log(num);
